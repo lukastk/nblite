1e64bcf6cafce8e7ccaea3918f62f067b0ab0f9b
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-03-16 12:34:34 +0000

    feat: can now disable pre commit hooks by setting environmnent vars

--- nblite/defaults/pre-commit.sh
+++ nblite/defaults/pre-commit.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-nbl clean
-nbl export
-nbl validate_staging
+if [ "$NBL_DISABLE_PC" = "true" ]; then
+    exit
+fi
+
+if [ "$NBL_DISABLE_PC_CLEAN" != "true" ]; then
+    nbl clean
+fi
+
+if [ "$NBL_DISABLE_PC_EXPORT" != "true" ]; then
+    nbl export
+fi
+
+if [ "$NBL_DISABLE_PC_VALIDATE" != "true" ]; then
+    nbl validate_staging
+fi
